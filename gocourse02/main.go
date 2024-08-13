e8e1bc7db91e4a8db8e9ac656e6b55947f35b924
Andrey Senchov <a.senchov@kevurugames.com>
2024-08-13 10:23:47 +0300

    add constructors

--- gocourse02/main.go
+++ gocourse02/main.go
@@ -5,62 +5,64 @@ import (
 )
 
 func main() {
-	zookyper := Zookyper{
-		Name:   "Mukola",
-		IsBusy: false,
-	}
+	zookyper := Zookeeper{}.New(false, "Mukola")
 	zookyper.PrintName()
 
-	cage := Cage{
-		Number:   1,
-		IsBroken: true,
-	}
-
+	cage := Cage{}.New(true, 1)
 	fmt.Printf("Cage number %d state =>%t\n", cage.Number, cage.IsBroken)
 	if !zookyper.IsBusy && cage.IsBroken {
 		cage.FixCage()
 	}
 	fmt.Printf("Cage number %d state =>%t\n", cage.Number, cage.IsBroken)
 
-	var sirko = Animal{
-		Name:   "Sirko",
-		Number: 1,
-	}
+	var sirko = Animal{}.New("Sirko", 1)
 	fmt.Printf("Animal with name %s is istting in cage number %d\n", sirko.Name, sirko.Number)
 
-	var sirko_son = sirko.Duplicate("Sirko junior", 1)
+	var sirko_son = Animal{}.New("Sirko junior", 1)
 	fmt.Printf("Animal with name %s is istting in cage number %d\n", sirko_son.Name, sirko_son.Number)
-
 }
 
-type Zookyper struct {
+type Zookeeper struct {
 	IsBusy bool
 	Name   string
 }
 
-func (z Zookyper) PrintName() {
+func (z Zookeeper) New(isBusy bool, name string) *Zookeeper {
+	return &Zookeeper{
+		IsBusy: isBusy,
+		Name:   name,
+	}
+}
+
+func (z Zookeeper) PrintName() {
 	fmt.Printf("Zookyper name is %v \n", z.Name)
 }
 
 type Cage struct {
 	IsBroken bool
-	Number   int32
+	Number   int
+}
+
+func (c Cage) New(isBroken bool, number int) *Cage {
+	return &Cage{
+		IsBroken: isBroken,
+		Number:   number,
+	}
 }
 
 func (c *Cage) FixCage() {
 	if c.IsBroken {
 		c.IsBroken = false
 	}
-
 }
 
 type Animal struct {
 	Name   string
-	Number int32
+	Number int
 }
 
-func (a Animal) Duplicate(name string, number int32) Animal {
-	return Animal{
+func (a Animal) New(name string, number int) *Animal {
+	return &Animal{
 		Name:   name,
 		Number: number,
 	}
