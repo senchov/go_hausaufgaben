8d6a597b9fa1151643cd370b785689b7b90225f7
Andrey Senchov <a.senchov@kevurugames.com>
2024-08-24 22:08:55 +0300

    add animals gourutine

---
+++ gocourse06/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("start")
+	data := make(chan string)
+	go CollectData(Animal{Name: "Dave", Mood: Sad, Hunger: 56, Health: 22}, data)
+	res := <-data
+	fmt.Println(res)
+}
+
+// Створіть горутину для кожної тварини в зоопарку. Кожна горутина збирає дані про стан тварини
+// (наприклад, рівень здоров'я, голод, настрій) і відправляє їх через канал до центральної системи моніторингу.
+type Mood string
+
+const (
+	Sad  Mood = "Sad"
+	Cool Mood = "Cool"
+
+	Undefined string = "Undefined"
+)
+
+type Animal struct {
+	Name   string
+	Health int
+	Hunger int
+	Mood
+}
+
+func CollectData(a Animal, data chan string) {
+	if a.Name == "" {
+		data <- Undefined
+		return
+	}
+
+	r := fmt.Sprintf("Animal->%s is mood->%s health->%v hunger->%v", a.Name, a.Mood, a.Health, a.Hunger)
+	data <- r
+}
---
+++ gocourse06/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCollectData(t *testing.T) {
+	daveData := make(chan string)
+	mikeData := make(chan string)
+	jackData := make(chan string)
+	undefinedData := make(chan string)
+	go CollectData(Animal{Name: "Dave", Mood: Sad, Hunger: 56, Health: 22}, daveData)
+	go CollectData(Animal{Name: "Mike", Mood: Cool, Hunger: 90, Health: 44}, mikeData)
+	go CollectData(Animal{Name: "Jack", Mood: Sad, Hunger: 0, Health: 56}, jackData)
+	go CollectData(Animal{Mood: Sad, Hunger: 0, Health: 56}, undefinedData)
+	daveStr := <-daveData
+	fmt.Println(daveStr)
+	if len(daveStr) == 0 {
+		t.Logf("Dave data is 0 expexted more than zero")
+	}
+
+	mikeStr := <-mikeData
+	fmt.Println(mikeStr)
+	if len(mikeStr) == 0 {
+		t.Logf("Mike data is 0 expexted more than zero")
+	}
+
+	jackStr := <-jackData
+	fmt.Println(jackStr)
+	if len(jackStr) == 0 {
+		t.Logf("Jack data is 0 expexted more than zero")
+	}
+
+	uStr := <-undefinedData
+	fmt.Println(uStr)
+	if uStr != Undefined {
+		t.Error("Data should be undefined if name is empthy")
+	}
+}
