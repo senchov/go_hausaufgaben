bf1743a315c7d50c399bd1326c59260fd9316de0
Andrey Senchov <a.senchov@kevurugames.com>
2024-08-31 21:11:00 +0300

    add cases to main

--- gocourse05/main.go
+++ gocourse05/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	records := []security.AnimalRecord{}
 	fmt.Println(records)
-	ptz := security.NewPTZCamera(0, 10)
+	ptz := security.NewPTZCamera(250, 10)
 	nv := security.NewNightvisionCamera(30, 6)
 
 	ptz.Record(*security.NewConditions(false, 2, 100), &records, *security.NewAnimalRecord(security.Bear, security.Sleep))
@@ -17,6 +17,12 @@ func main() {
 	ptz.Record(*security.NewConditions(false, 5, 100), &records, *security.NewAnimalRecord(security.Rat, security.Eat))
 	nv.Record(*security.NewConditions(true, 50, 100), &records, *security.NewAnimalRecord(security.Tiger, security.Walking))
 	nv.Record(*security.NewConditions(true, 3, 100), &records, *security.NewAnimalRecord(security.Tiger, security.Walking))
+	ptz.Record(*security.NewConditions(false, 150, 100), &records, *security.NewAnimalRecord(security.Rat, security.Walking))
+	ptz.Record(*security.NewConditions(false, 4, 100), &records, *security.NewAnimalRecord(security.Bear, security.Sleep))
+	nv.Record(*security.NewConditions(true, 55, 0), &records, *security.NewAnimalRecord(security.Tiger, security.Walking))
+	ptz.Record(*security.NewConditions(false, 27, 100), &records, *security.NewAnimalRecord(security.Rat, security.Eat))
+	nv.Record(*security.NewConditions(true, 33, 100), &records, *security.NewAnimalRecord(security.Rat, security.Walking))
+	nv.Record(*security.NewConditions(true, 3, 100), &records, *security.NewAnimalRecord(security.Bear, security.Eat))
 
 	for _, v := range records {
 		fmt.Println(v)
