9f37b630c2a11f2cd6c328fc9803f7cdced8cb73
Andrey Senchov <a.senchov@kevurugames.com>
2024-09-01 11:04:59 +0300

    add to main

--- gocourse06/main.go
+++ gocourse06/main.go
@@ -3,11 +3,68 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("start")
-	data := make(chan string)
-	go CollectData(Animal{Name: "Dave", Mood: Sad, Hunger: 56, Health: 22}, data)
-	res := <-data
-	fmt.Println(res)
+	joData := make(chan string)
+	timData := make(chan string)
+	jackData := make(chan string)
+	undefinedData := make(chan string)
+	go CollectData(Animal{Name: "Jo", Mood: Sad, Hunger: 42, Health: 11}, joData)
+	go CollectData(Animal{Name: "Tim", Mood: Cool, Hunger: 21, Health: 44}, timData)
+	go CollectData(Animal{Name: "Jack", Mood: Sad, Hunger: 16, Health: 8}, jackData)
+	go CollectData(Animal{Mood: Sad, Hunger: 0, Health: 56}, undefinedData)
+
+	daveStr := <-joData
+	fmt.Println(daveStr)
+
+	mikeStr := <-timData
+	fmt.Println(mikeStr)
+
+	jackStr := <-jackData
+	fmt.Println(jackStr)
+
+	uStr := <-undefinedData
+	fmt.Println(uStr)
+
+	full := make(chan int, 2)
+	empty := make(chan int, 1)
+	go CheckTrough(Trough{ID: 1, Amount: 100}, full, empty)
+	go CheckTrough(Trough{ID: 2, Amount: 60}, full, empty)
+	go CheckTrough(Trough{ID: 3, Amount: 40}, full, empty)
+	go CheckTrough(Trough{ID: 4, Amount: 0}, full, empty)
+
+	fullIds := []int{<-full, <-full}
+	for i, v := range fullIds {
+		fmt.Printf("full id=>%d index=%d \n", v, i)
+	}
+
+	emptyIds := []int{<-empty, <-empty}
+	for i, v := range emptyIds {
+		fmt.Printf("empty id=>%d index=%d\n", v, i)
+	}
+
+	firstCageLock := make(chan bool)
+	secondCageLock := make(chan bool)
+	thirdCageLock := make(chan bool)
+	fourthCageLock := make(chan bool)
+	go IsShouldOpenCage(5, firstCageLock)
+	go IsShouldOpenCage(10, secondCageLock)
+	go IsShouldOpenCage(12, thirdCageLock)
+	go IsShouldOpenCage(3, fourthCageLock)
+	first := <-firstCageLock
+	PrintLockState(first, "First")
+	second := <-secondCageLock
+	PrintLockState(second, "Second")
+	third := <-thirdCageLock
+	PrintLockState(third, "Third")
+	fourth := <-fourthCageLock
+	PrintLockState(fourth, "Fourth")
+}
+
+func PrintLockState(isOpen bool, lockName string) {
+	if isOpen {
+		fmt.Printf("%s lock is open \n", lockName)
+	} else {
+		fmt.Printf("%s lock is closed \n", lockName)
+	}
 }
 
 // Створіть горутину для кожної тварини в зоопарку. Кожна горутина збирає дані про стан тварини
